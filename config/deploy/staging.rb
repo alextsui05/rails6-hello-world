4c468b36e800638be7255aea5326f934e0200560
Alex Tsui <alextsui@pm.me>
2020-05-23 09:21:50 -0700

    Configure capistrano files

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,6 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server 'x.atsui.dev', user: 'atsui', roles: %w{app}
 
 
 # role-based syntax
