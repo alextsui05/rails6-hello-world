f0cd7e00c645db661febab633b09a161c15d4a11
Alex Tsui <alextsui@pm.me>
2020-05-24 15:35:01 -0700

    :ok_hand: Add email config for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -61,6 +61,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "hello_world_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'x.atsui.dev', protocol: 'https' }
+  config.action_mailer.smtp_settings = {
+    openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
